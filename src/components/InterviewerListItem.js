9bed777c3a18289e841ed75fa88c89d6fb01ceeb
David Braverman <braverman.david.business@gmail.com>
2022-08-09 21:58:33 -0400

    Create base for InterviewerList files, create InterviewerListItem display and functionality.

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import classNames from "classnames";
+
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem (props) {
+
+  const displayName = () => {
+    if (props.selected) {
+      return props.name;
+    }
+  }
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item-image": props.avatar,
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
+      <img 
+        className={interviewerClass}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {displayName()}
+    </li>      
+  );
+}
+
