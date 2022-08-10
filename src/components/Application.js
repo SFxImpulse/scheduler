ddb1ec330250bd496f77c6cfd7205a526c3c5a28
David Braverman <braverman.david.business@gmail.com>
2022-08-10 12:10:45 -0400

    Add functionality to InterviewerList, refactor onClick functions.

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,25 +4,25 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application (props) {
+
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
 
   const [day, setDay] = useState("Monday");
 
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -14,7 +14,7 @@ export default function DayList (props) {
         setDay={props.setDay}
       />
     );
-  })
+  });
 
   return (
     <ul>
