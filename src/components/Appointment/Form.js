707a22822d51a86099ecf9f9f4220480763bcc36
David Braverman <braverman.david.business@gmail.com>
2022-08-10 20:22:06 -0400

    Add header, show, and empty forms into AppointmentIndex file.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -11,12 +11,17 @@ export default function Form (props) {
   const reset = () => {
     setStudent("");
     setInterviewer(null);
-  }
+  };
+
+  const cancel = () => {
+    props.onCancel(); 
+    reset();
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input 
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -38,7 +43,7 @@ export default function Form (props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={reset}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
