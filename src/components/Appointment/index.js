9d4164e2e1c5a4bd91ca0342e0c2bf5fd707327c
David Braverman <braverman.david.business@gmail.com>
2022-08-12 17:16:46 -0400

    Add custom hook to application with transitions to create interview and back.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,28 +5,45 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment (props) {
+
+  const { mode, transition, back } = useVisualMode (
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header 
         time={props.time}
       />
-      {
-        props.interview
-        ? 
-        <Show 
+      { mode === EMPTY && (
+        <Empty 
+          onAdd={() => {
+            transition(CREATE);
+          }} 
+        /> 
+      )}
+      { mode === SHOW && (
+        <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onEdit={props.onEdit}
-          onDelete={props.onDelete}
         />
-        :
-        <Empty
-          onAdd={props.onAdd}
+      )}
+      { mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => {
+            back(EMPTY)
+          }}
         />
-      }
+      )}
     </article>
   );
 }
