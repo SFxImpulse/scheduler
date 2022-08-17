55eb5d94f3f5d466745ae315ec238dd2132e4b4e
David Braverman <braverman.david.business@gmail.com>
2022-08-17 11:15:40 -0400

    Add tests to form, and application components, format files.

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,21 +3,32 @@ import React from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index";
 
-import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
+import {
+  getAppointmentsForDay,
+  getInterviewersForDay,
+  getInterview,
+} from "helpers/selectors";
 import useApplicationData from "hooks/useApplicationData";
 
 import "components/Application.scss";
 
-export default function Application (props) {
+export default function Application(props) {
 
-  const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
-  
+  // Retrieving functions from custom hook useApplicationData.
+  const { state,
+    setDay,
+    bookInterview,
+    cancelInterview 
+  } = useApplicationData();
+
+  // Setting interviewers array.
   const interviewers = getInterviewersForDay(state, state.day);
-  
+
+  // Setting appointments array.
   const appointments = getAppointmentsForDay(state, state.day).map(
-    appointment => {
+    (appointment) => {
       return (
-        <Appointment 
+        <Appointment
           key={appointment.id}
           {...appointment}
           interview={getInterview(state, appointment.interview)}
@@ -29,27 +40,24 @@ export default function Application (props) {
     }
   );
 
+  // The Application component
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-        className="sidebar--centered"
-        src="images/logo.png"
-        alt="Interview Scheduler"
-      />
-      <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu">
-        <DayList
-          days={state.days}
-          value={state.day}
-          onChange={setDay}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
         />
-      </nav>
-      <img
-        className="sidebar__lhl sidebar--centered"
-        src="images/lhl.png"
-        alt="Lighthouse Labs"
-      />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={state.days} value={state.day} onChange={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {appointments}
@@ -57,4 +65,4 @@ export default function Application (props) {
       </section>
     </main>
   );
-};
+}
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,25 +1,22 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function useApplicationData () {
-
-  
+export default function useApplicationData() {
   // Assigns state object, and setState function.
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
   });
-  
+
   // Dynamically updates the spots remaining for each day.
   const updateSpots = (day, days, appointments) => {
-    
-    const foundDay = days.find(d => d.name === day);
+    const foundDay = days.find((d) => d.name === day);
 
     if (!foundDay) {
       return 0;
     }
-    
+
     let count = 0;
 
     for (const id of foundDay.appointments) {
@@ -29,75 +26,96 @@ export default function useApplicationData () {
       }
     }
 
-    const output = { ...foundDay, spots: count };
-
-    const newDays = days.map((d) => d.name === day ? output : d);
+    const newDay = { ...foundDay, spots: count };
+    const newDays = days.map((d) => (d.name === day ? newDay : d));
 
     return newDays;
-
   };
 
   // Other solution
-  // const updateSpots = (state, days, id) => {
+  /* 
+  
+  const updateSpots = (state, appointments, id) => {
 
-  // }
+    const dayObj = state.days.find(d => d.name === state.day);
+
+    let spots = 0;
+
+    for (const id of dayObj.appointments) {
+      const appointment = appointments[id];
+      if (!appointment.interview) {
+        spots += 1;
+      }
+    }
+
+    const newDay = {...dayObj, spots};
+    const newDays = state.days.map(d => d.name === state.day ? newDay : d)
+
+    return days;
+    
+  };
+
+    */
 
   // Defines setDay function as to set the state of the current day.
-  const setDay = day => setState(prev => ({ ...prev, day }));
+  const setDay = (day) => setState((prev) => ({ ...prev, day }));
 
   // Retrieves data from multiple APIs.
   useEffect(() => {
     Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
     });
   }, []);
 
   // Makes an axios put request to assign an interview object to an appointment object.
-  function bookInterview (id, interview) {
+  function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview }
+      interview: { ...interview },
     };
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
-    return axios.put(`/api/appointments/${id}`, { interview })
-    .then(() => { 
-        const newDays = updateSpots(state.day, state.days, appointments);
-        setState({
-          ...state,
-          appointments,
-          days: newDays
-        });
+    return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
+      // Calls the return value for updateSpots.
+      const newDays = updateSpots(state.day, state.days, appointments);
+      setState({
+        ...state,
+        appointments,
+        days: newDays,
       });
-  };
+    });
+  }
 
   // Makes an axios delete request to delete an interview object from an appointment object.
-  function cancelInterview (id, interview) {
+  function cancelInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
-      interview: null
+      interview: null,
     };
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {
-        const newDays = updateSpots(state.day, state.days, appointments);
-        setState({
-          ...state,
-          appointments,
-          days: newDays
-        });
+    return axios.delete(`/api/appointments/${id}`).then(() => {
+      const newDays = updateSpots(state.day, state.days, appointments);
+      setState({
+        ...state,
+        appointments,
+        days: newDays,
       });
-  };
+    });
+  }
 
   return { state, setDay, bookInterview, cancelInterview };
-
-};
+}
